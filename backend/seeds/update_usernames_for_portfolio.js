d83ff4a533d3005ccb99af85228fd8cfd9061aa6
ChanGoza <gozyosan.ko@gmail.com>
2025-11-11 08:40:26 +0900

    update username for portfolio

---
+++ backend/seeds/update_usernames_for_portfolio.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  await knex("users").where("username","ゆきまんこ").update({username:"ゆきや"})
+  console.log("ポートフォリオ用にユーザー名を更新しました。")
+  const userExists=await knex('users').where("username","test").first();
+  if(userExists){
+    console.log("User 'test' already exists. No changes made.");
+  }else{
+    const hashedPassword = await require('bcrypt').hash('1234test', 10);
+    await knex('users').insert({
+      username:"test",
+      password:hashedPassword,
+      role:"user"
+    });
+    console.log("User 'test' has been added.");
+  }
+};
